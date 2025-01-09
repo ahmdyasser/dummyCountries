f0b5ccc808560a46c7df8c1a0ab61e6e7ffb8e04
ahmdyasser <ahmadyas_ser@icloud.com>
2025-01-09 13:55:35 +0200

    Initial Commit

---
+++ dummyCountries/dummyCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyCountriesApp.swift
+//  dummyCountries
+//
+//  Created by Ahmad Yasser on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct dummyCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dummyCountriesTests/dummyCountriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  dummyCountriesTests.swift
+//  dummyCountriesTests
+//
+//  Created by Ahmad Yasser on 09/01/2025.
+//
+
+import Testing
+@testable import dummyCountries
+
+struct dummyCountriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
