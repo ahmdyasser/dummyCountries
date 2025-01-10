81e1796e3dee4d8af9d649bb187ac4c373ec6d28
ahmdyasser <ahmadyas_ser@icloud.com>
2025-01-10 02:25:56 +0200

    Add URL cache

--- dummyCountries/Features/Home/BusinessLogic/CountryFetchingUseCase.swift
+++ dummyCountries/Features/Home/BusinessLogic/CountryFetchingUseCase.swift
@@ -35,11 +35,18 @@ protocol NetworkService {
     func fetch<T: Decodable>(from urlString: String) async throws -> T
 }
 
-class DefaultNetworkService: NetworkService {
-    private let urlSession: URLSession
+class CachingNetworkService: NetworkService {
+    private let cache: URLCache
+    private let session: URLSession
     
-    init(urlSession: URLSession = .shared) {
-        self.urlSession = urlSession
+    init(cacheSize: Int = 50 * 1024 * 1024) { // 50MB cache
+        let config = URLSessionConfiguration.default
+        cache = URLCache(memoryCapacity: cacheSize / 2,
+                        diskCapacity: cacheSize,
+                        diskPath: "countries_cache")
+        config.urlCache = cache
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        session = URLSession(configuration: config)
     }
     
     func fetch<T: Decodable>(from urlString: String) async throws -> T {
@@ -47,13 +54,25 @@ class DefaultNetworkService: NetworkService {
             throw CountryFetchingError.invalidURL
         }
         
+        let request = URLRequest(url: url)
+        
         do {
-            let (data, _) = try await urlSession.data(from: url)
-            let decodedData = try JSONDecoder().decode(T.self, from: data)
-            return decodedData
-        } catch let error as DecodingError {
-            throw CountryFetchingError.decodingError(error)
+            let (data, response) = try await session.data(for: request)
+            
+            // Store response in cache if it's valid
+            if let httpResponse = response as? HTTPURLResponse,
+               200...299 ~= httpResponse.statusCode {
+                cache.storeCachedResponse(
+                    CachedURLResponse(response: response, data: data),
+                    for: request
+                )
+            }
+            
+            return try JSONDecoder().decode(T.self, from: data)
         } catch {
+            if let cachedResponse = cache.cachedResponse(for: request) {
+                return try JSONDecoder().decode(T.self, from: cachedResponse.data)
+            }
             throw CountryFetchingError.networkError(error)
         }
     }
@@ -68,7 +87,7 @@ class DefaultCountryFetchingUseCase: CountryFetchingUseCase {
     private let networkService: NetworkService
     private let baseURL = "https://restcountries.com/v2/all"
     
-    init(networkService: NetworkService = DefaultNetworkService()) {
+    init(networkService: NetworkService = CachingNetworkService()) {
         self.networkService = networkService
     }
     
